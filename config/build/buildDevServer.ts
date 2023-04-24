f31220f27c2e9e4abb111a77b9f77c42b0422e88
Sergey Malcev <malcqq@gmail.com>
2023-04-25 02:55:54 +0300

    add hmr, react-refresh-webpack-plugin

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -6,5 +6,6 @@ export function buldDevServer(options: BuildOptions): DevServerConfiguration {
     port: options.port,
     open: true,
     historyApiFallback: true,
+    hot: true,
   };
 }
--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,4 +1,5 @@
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+// import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 import webpack from 'webpack';
 import HTMLWebpackPlugin from 'html-webpack-plugin';
 import { BuildOptions } from './types/config';
@@ -7,7 +8,7 @@ export function buildPlugins({
   paths,
   isDev,
 }: BuildOptions): webpack.WebpackPluginInstance[] {
-  return [
+  const plugins = [
     new HTMLWebpackPlugin({
       template: paths.html,
     }),
@@ -20,4 +21,10 @@ export function buildPlugins({
       __IS_DEV__: JSON.stringify(isDev),
     }),
   ];
+  if (isDev) {
+    // plugins.push(new ReactRefreshWebpackPlugin());
+    plugins.push(new webpack.HotModuleReplacementPlugin());
+  }
+
+  return plugins;
 }
