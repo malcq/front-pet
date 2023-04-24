4fc393c92197c229dad51061d1bbc45c95fdce0b
Sergey Malcev <malcqq@gmail.com>
2023-04-25 02:33:59 +0300

    connect i18n in App

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,17 +5,21 @@ import { useTheme } from 'app/providers/ThemeProvider';
 import { AppRouter } from './providers/router';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
+import { Suspense } from 'react';
 
 const App = () => {
   const { theme } = useTheme();
 
   return (
     <div className={classNames('app', { hovered: false }, [theme])}>
-      <Navbar />
-      <div className='content-page'>
-        <Sidebar />
-        <AppRouter />
-      </div>
+      <Suspense fallback=''>
+        <Navbar />
+
+        <div className='content-page'>
+          <Sidebar />
+          <AppRouter />
+        </div>
+      </Suspense>
     </div>
   );
 };
