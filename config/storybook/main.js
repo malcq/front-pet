334e510c47eccb6f348bc53e7692f72185e90c10
Sergey Malcev <malcqq@gmail.com>
2023-05-02 01:59:39 +0300

    add  loki, docker

--- config/storybook/main.js
+++ config/storybook/main.js
@@ -7,6 +7,9 @@ const config = {
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
   ],
+  features: {
+    storyStoreV7: true,
+  },
   staticDirs: ['../../public'], // set locales IMPORTANT
   framework: {
     name: '@storybook/react-webpack5',
