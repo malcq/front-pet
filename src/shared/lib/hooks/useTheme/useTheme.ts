b3304b49c020f340c58825eec03c657edbc748e4
malcq <malcqq@gmail.com>
2023-07-16 03:15:04 +0300

    add json settings

--- src/shared/lib/hooks/useTheme/useTheme.ts
+++ src/shared/lib/hooks/useTheme/useTheme.ts
@@ -1,18 +1,17 @@
 import { useContext } from 'react';
 
-import { LOCAL_STORAGE_THEME_KEY } from '../../../const/localstorage';
 import { Theme } from '../../../const/theme';
 import { ThemeContext } from '../../context/ThemeContext';
 
 interface UseThemeResult {
-  toggleTheme: () => void;
+  toggleTheme: (saveAction: (theme: Theme) => void) => void;
   theme: Theme;
 }
 
 export function useTheme(): UseThemeResult {
   const { theme, setTheme } = useContext(ThemeContext);
 
-  const toggleTheme = () => {
+  const toggleTheme = (saveAction: (theme: Theme) => void) => {
     let newTheme: Theme;
     switch (theme) {
       case Theme.DARK:
@@ -28,7 +27,7 @@ export function useTheme(): UseThemeResult {
         newTheme = Theme.LIGHT;
     }
     setTheme?.(newTheme);
-    localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
+    saveAction?.(newTheme);
   };
 
   return {
